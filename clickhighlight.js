6892d4366a64f03f3f1dedd4bcd9bb99fe003611
Chris Heilmann <chris.heilmann@gmail.com>
2012-04-23 10:29:49 +0100

    syntax

--- clickhighlight.js
+++ clickhighlight.js
@@ -6,76 +6,71 @@
   http://christianheilmann.com/license.txt
 */
 clickhighlight = function() {
-  var classes = {
-    nohighlight: 'nohighlight'
-  };
-  var styles = {
-    size: '100px',
-    duration:  '1s',
-    colour:    'rgba( 255, 10, 10, 0.7 )'
-  };
-  function init( o ) {
-    if ( plot ) { plot.parentNode.removeChild(plot); }
-    if ( o ) {
-      if ( o && o.size ) { styles.size = o.size; }
-      if ( o && o.duration ) { styles.duration = o.duration; }
-      if ( o && o.colour ) { styles.colour = o.colour; }
-      if ( o && o.nohighlight ) { classes.nohighlight  = o.nohighlight; }
-    }
-    var plot = document.createElement( 'div' ),
+  
+  function init(o) {
+    if (plot) { plot.parentNode.removeChild(plot); }
+
+    var plot = document.createElement('div'),
+        size     = (o && o.size)     || '100px',
+        duration = (o && o.duration) || '1s',
+        colour   = (o && o.colour)   || 'rgba(255, 10, 10, 0.7)',
+        noclickclass = (o && o.nohighlight) || 'nohighlight',
         pressed = false,
-        offset = parseInt( styles.size, 10 ) / 2;
-        prefix = getprefix( plot );
+        offset = parseInt(size, 10) / 2;
+        prefix = getprefix(plot);
 
-    if ( prefix !== null ) {
-      plot.style.borderRadius = styles.size;
-      plot.style.background = styles.colour;
+    if (prefix !== null) {
+      plot.style.borderRadius = size;
+      plot.style.background = colour;
       plot.style.pointerEvents = 'none';
-      plot.style.width = styles.size;
-      plot.style.height = styles.size;
+      plot.style.width = size;
+      plot.style.height = size;
       plot.style.position = 'absolute';
-      plot.style[ prefix + 'Transform' ] = 'scale( 0, 0 )';
-      plot.style[ prefix + 'Transition' ] = '-' + prefix.toLowerCase() + '-' +
-                                            'transform ' + styles.duration;
-      document.body.appendChild( plot );
+      plot.style[prefix + 'Transform'] = 'scale(0, 0)';
+      plot.style[prefix + 'Transition'] = '-' + prefix.toLowerCase() + '-' +
+                                            'transform ' + duration;
+      document.body.appendChild(plot);
 
-      function down( ev ) {
+      function down(ev) {
         var t = ev.target.classList; 
-        if ( !t.contains( classes.nohighlight ) ) {
+        if (!t.contains(noclickclass)) {
           pressed = true;
-          move( ev );
-          plot.style[ prefix + 'Transform' ] = 'scale( 1, 1 )';
+          move(ev);
+          plot.style[prefix + 'Transform'] = 'scale(1, 1)';
         }
       };
 
-      function up( ev ) {
+      function up(ev) {
         pressed = false;
-        plot.style[ prefix + 'Transform' ] = 'scale( 0, 0 )';
+        plot.style[prefix + 'Transform'] = 'scale(0, 0)';
       };
 
-      function move( ev ) {
-        if ( pressed ) { 
-          var x =  ev.pageX, 
+      function move(ev) {
+        if (pressed) { 
+          var x = ev.pageX, 
               y = ev.pageY;
           plot.style.left = x - offset + 'px';
           plot.style.top = y - offset + 'px';
         }
       };
 
-      document.addEventListener( 'mousedown', down , false );
-      document.addEventListener( 'mouseup', up , false );
-      document.addEventListener( 'mousemove', move, false );  
-      document.addEventListener( 'touchstart', down , false );
-      document.addEventListener( 'touchend', up , false );
-      document.addEventListener( 'touchmove', move, false );  
+      document.addEventListener('mousedown', down , false);
+      document.addEventListener('mouseup', up , false);
+      document.addEventListener('mousemove', move, false);
+      /* TODO 
+      document.addEventListener('touchstart', down , false);
+      document.addEventListener('touchend', up , false);
+      document.addEventListener('touchmove', move, false);  
+      */
     }
   };
+  
   function getprefix(elm) {
-    var prefixes = [ 'Moz', 'Webkit', 'Khtml', 'O', 'ms', '' ],
+    var prefixes = ['Moz', 'Webkit', 'Khtml', 'O', 'ms', ''],
         all = prefixes.length,
         prefix = null;
-    while ( all-- ) {
-      if ( elm.style[ prefixes[all] + 'Transition' ] !== undefined ) {
+    while (all--) {
+      if (elm.style[ prefixes[ all ] + 'Transition' ] !== undefined) {
         prefix = prefixes[all];
         break;
       }
